31fe99fc193fbba78341c7c3c9428df83cca9e9b
alex <lescot.alexg@gmail.com>
2025-04-29 11:21:58 +0200

    creation de la structure de base de la Timeline Les timelines objects ont tout les attributs prévus

--- src/timeline/TimelineObject.java
+++ src/timeline/TimelineObject.java
@@ -1,4 +1,27 @@
 package timeline;
 
 public class TimelineObject {
+    /* Information sur l'objet */
+    String name;
+    int clipId;
+    String mediaType; // Deux options video ou audio
+    int srcId; // Quand il y a plusieurs sources videos/audios
+    /* Information sur sa positon dans la timeline */
+    long[] offset;
+    long[] start;
+    long[] duration;
+
+    TimelineObject(String name, int clipId, int srcId, long[] offset, long[] start, long[] duration) {
+        this.name = name;
+        this.clipId = clipId;
+        this.mediaType = "video"; // en vrai on appellera la classe clip pour avoir l'info
+        this.srcId = srcId;
+        this.offset = offset;
+        this.start = start;
+        this.duration = duration;
+    }
+
+    TimelineObject(String name, int clipId, long[] offset, long[] start, long[] duration) {
+        this(name, clipId, 1, offset, start, duration);
+    }
 }
