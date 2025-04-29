9bac68b8e5f22ee10b5bb96b9e9f125cc0d9f8c6
alex <lescot.alexg@gmail.com>
2025-04-29 11:55:41 +0200

    ajout des setter pour les TimelinesObjects

--- src/timeline/TimelineObject.java
+++ src/timeline/TimelineObject.java
@@ -2,14 +2,14 @@ package timeline;
 
 public class TimelineObject {
     /* Information sur l'objet */
-    String name;
-    int clipId;
-    String mediaType; // Deux options video ou audio
-    int srcId; // Quand il y a plusieurs sources videos/audios
+    private String name;
+    private int clipId;
+    private String mediaType; // Deux options video ou audio
+    private int srcId; // Quand il y a plusieurs sources videos/audios
     /* Information sur sa positon dans la timeline */
-    long[] offset;
-    long[] start;
-    long[] duration;
+    private long[] offset;
+    private long[] start;
+    private long[] duration;
 
     TimelineObject(String name, int clipId, int srcId, long[] offset, long[] start, long[] duration) {
         this.name = name;
@@ -24,4 +24,23 @@ public class TimelineObject {
     TimelineObject(String name, int clipId, long[] offset, long[] start, long[] duration) {
         this(name, clipId, 1, offset, start, duration);
     }
+
+    public void setClipId(int clipId) {
+        this.clipId = clipId;
+    }
+
+    public void setSrcId(int srcId) {
+        this.srcId = srcId;
+    }
+
+    public void setOffset(long[] offset) {
+        this.offset = offset;
+    }
+    public void setStart(long[] start) {
+        this.start = start;
+    }
+    public void setDuration(long[] duration) {
+        this.duration = duration;
+    }
+
 }
