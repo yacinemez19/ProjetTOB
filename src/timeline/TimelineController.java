179c6dbbb55429fc7e29cf33ef470e313301d6c2
alex <lescot.alexg@gmail.com>
2025-04-30 10:27:07 +0200

    interface timeline

--- src/timeline/TimelineController.java
+++ src/timeline/TimelineController.java
@@ -29,6 +29,7 @@ public class TimelineController {
             }
             timelineSplitPane.setDividerPositions(positions);
 
+
         } catch (IOException e) {
             e.printStackTrace();
         }
