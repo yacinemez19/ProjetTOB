9fa291dd0d26c8f5fe29b70786ed9a9360ed8ea3
dbt3986 <doryan.benoit@etu.inp-n7.fr>
2025-05-03 16:58:03 +0200

    MAJ TrackManager sur la javadoc + le remove track où il vérifie que la track est bien présente avant de la supprimer.

--- src/TrackManager.java
+++ src/TrackManager.java
@@ -7,16 +7,35 @@ public class TrackManager {
 
     private ArrayList<Track> tracks;
 
+    /**
+     * getter liste des tracks
+     * @return ArrayList<Track> tracks
+     */
     public ArrayList<Track> getTracks() {
+
         return this.tracks;
     }
 
+    /**
+     * ajoute une track
+     * @param track
+     */
     public void addTrack(Track track) {
+
         this.tracks.add(track);
     }
 
-    public boolean removeTrack(Track track) {
-        return this.tracks.remove(track);
+    /**
+     * supprime la track de la liste
+     * @param track
+     */
+    public void removeTrack(Track track) {
+        if (this.tracks.contains(track)) {
+            this.tracks.remove(track);
+        }else {
+            System.out.println("La track n'existe pas");
+        }
+
     }
 
     public void moveTrack(Track track) {
@@ -24,15 +43,29 @@ public class TrackManager {
         System.out.println("move track");
     }
 
+    /**
+     * Fonction retournant la taille de la liste des tracks
+     * @return size
+     */
     public int size(){
+
         return this.tracks.size();
     }
 
+    /**
+     * Fonction retournant la durée max des tracks
+     * @return maxDuration
+     */
     public Duration getMaxDuration() {
+
         Duration maxDuration = Duration.ZERO;
+
         for (Track track : this.tracks) {
+
             Duration trackTotalDuration = track.getTotalDuration();
+
             if (trackTotalDuration.compareTo(maxDuration) > 0) {
+
                 maxDuration = trackTotalDuration;
             }
         }
