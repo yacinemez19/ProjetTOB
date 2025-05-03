6168a819638d236563619fdaa3b9df7336d1b6c4
Yacine Meziani <135065821+yacinemez19@users.noreply.github.com>
2025-05-03 19:51:54 +0200

    Add MenuBarController to handle project saving functionality

---
+++ src/MenuBarController.java
@@ -0,0 +1,32 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+
+import java.nio.file.Path;
+
+public class MenuBarController implements Initializable {
+
+    private VideoProject videoProject;
+
+    @FXML
+    public void saveProject() {
+        // Implémentez la logique pour sauvegarder le projet ici
+        System.out.println("Sauvegarde du projet...");
+        // Mettre saveFilePath à la racine du projet
+        Path saveFilePath = Paths.get(videoProject.getProjectName() + ".json");
+
+        videoProject.saveProject(saveFilePath);
+    }
+
+    public void setVideoProject(VideoProject videoProject) {
+        this.videoProject = videoProject;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
