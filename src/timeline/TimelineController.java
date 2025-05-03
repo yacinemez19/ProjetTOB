e7533f7faaf198bbf0d3532017e595d5dc996c5e
Sophie <sophie.girardot64@gmail.com>
2025-05-03 12:29:05 +0200

    premierversiontimer

--- src/timeline/TimelineController.java
+++ src/timeline/TimelineController.java
@@ -1,10 +1,16 @@
 package timeline;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.Node;
+import javafx.util.Duration;
+import org.freedesktop.gstreamer.ClockTime;
+import org.freedesktop.gstreamer.Format;
 
 import java.io.IOException;
 
@@ -19,6 +25,7 @@ public class TimelineController {
             addTrack("Track 1");
             addTrack("Track 2");
             addTrack("Audio 1");
+            initializeTimer();
 
             // Répartir équitablement les tracks dans l'espace alloué
             int trackCount = timelineSplitPane.getItems().size();
@@ -53,4 +60,14 @@ public class TimelineController {
     private void separer() {
 
     }
+
+    // Label pour afficher le temps écoulé
+    @FXML
+    private Label timerLabel;
+
+    @FXML
+    public void initializeTimer() {
+        timerLabel.setText("00:00:00");
+    }
+
 }
