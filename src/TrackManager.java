70382bb04111fde727c889573d4d6c5200aa8af8
dbt3986 <doryan.benoit@etu.inp-n7.fr>
2025-05-03 16:28:58 +0200

    Premiere implémentation de TrackManager

--- src/TrackManager.java
+++ src/TrackManager.java
@@ -1,2 +1,38 @@
+import timeline.Track;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrackManager {
+
+    private ArrayList<Track> tracks;
+
+    public ArrayList<Track> getTracks() {
+        return this.tracks;
+    }
+
+    public void addTrack(Track track) {
+        this.tracks.add(track);
+    }
+
+    public boolean removeTrack(Track track) {
+        return this.tracks.remove(track);
+    }
+
+    public void moveTrack(Track track) {
+        // à voir si on garde ou pas
+        System.out.println("move track");
+    }
+
+    public int size(){
+        return this.tracks.size();
+    }
+
+    public Duration getMaxDuration() {
+        System.out.println("Max duration :");
+        return null;
+    }
+
+
 }
