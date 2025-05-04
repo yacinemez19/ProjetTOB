01bdfb6b2828cb03d3c214fb46eca977d0bb664b
alex <lescot.alexg@gmail.com>
2025-05-04 17:17:43 +0200

    cleanup

--- src/MainController.java
+++ src/MainController.java
@@ -5,8 +5,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
 
 public class MainController {
 
