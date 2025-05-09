7d23175008cc059a81203ce3c935ca98d2ace548
alex <lescot.alexg@gmail.com>
2025-05-09 12:26:14 +0200

    TimelineObject acutalisé

--- src/com/timeline/TimelineObject.java
+++ src/com/timeline/TimelineObject.java
@@ -2,46 +2,33 @@ package com.timeline;
 
 import java.util.HashMap;
 
+import com.Clip;
+
 public class TimelineObject {
     /* Information sur l'objet */
+    private Clip source;
     private String name;
-    private int clipId;
     private String mediaType; // Deux options video ou audio
-    private int srcId; // Quand il y a plusieurs sources videos/audios
     /* Information sur sa positon dans la com.timeline */
-    private long[] offset;
-    private long[] start;
-    private long[] duration;
+    private long offset;
+    private long start;
+    private long duration;
 
-    TimelineObject(String name, int clipId, int srcId, long[] offset, long[] start, long[] duration) {
-        this.name = name;
-        this.clipId = clipId;
+    TimelineObject(Clip source, String mediaType, long offset, long start) {
+        this.name = source.getName();
         this.mediaType = "video"; // en vrai on appellera la classe clip pour avoir l'info
-        this.srcId = srcId;
-        this.offset = offset;
+        this.offset = offset; // TODO : ca veut dire quoi offset ?
         this.start = start;
-        this.duration = duration;
+        this.duration = source.getDuration().toNanos();
     }
 
-    TimelineObject(String name, int clipId, long[] offset, long[] start, long[] duration) {
-        this(name, clipId, 1, offset, start, duration);
-    }
-
-    public void setClipId(int clipId) {
-        this.clipId = clipId;
-    }
-
-    public void setSrcId(int srcId) {
-        this.srcId = srcId;
-    }
-
-    public void setOffset(long[] offset) {
+    public void setOffset(long offset) {
         this.offset = offset;
     }
-    public void setStart(long[] start) {
+    public void setStart(long start) {
         this.start = start;
     }
-    public void setDuration(long[] duration) {
+    public void setDuration(long duration) {
         this.duration = duration;
     }
 
@@ -49,40 +36,32 @@ public class TimelineObject {
         return name;
     }
 
-    public int getClipId() {
-        return clipId;
-    }
-
     public String getMediaType() {
         return mediaType;
     }
 
-    public int getSrcId() {
-        return srcId;
-    }
-
-    public long[] getOffset() {
+    public long getOffset() {
         return offset;
     }
 
-    public long[] getStart() {
+    public long getStart() {
         return start;
     }
 
-    public long[] getDuration() {
+    public long getDuration() {
         return duration;
+    }
+
+    public Clip getSource() {
+        return source;
     }
 
     public String toString(){
         HashMap<String,Object> data = new HashMap<>();
-        data.put("name", name);
-        data.put("clipId", clipId);
+        data.put("clip", source);
         data.put("mediaType", mediaType);
-        data.put("srcId", srcId);
         data.put("offset", offset);
         data.put("start", start);
         return data.toString();
     }
-
-
 }
